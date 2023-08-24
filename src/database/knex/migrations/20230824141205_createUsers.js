c5754059a1c4fdc6340cbbd74721de81d36ec02f
arthur <arthur.rios007@gmail.com>
2023-08-24 11:51:30 -0300

    run migrate users

---
+++ src/database/knex/migrations/20230824141205_createUsers.js
@@ -0,0 +1,22 @@
+exports.up = async (knex) => {
+  const exists = await knex.schema.hasTable("users")
+
+  if (!exists) {
+    await knex.schema
+      .createTable("users", table => {
+      table.increments("id");
+      table.text("name");
+      table.text("email");
+      table.text("password");
+      table.text("avatar").nullable();
+
+      table.timestamp("create_at").default(knex.fn.now());
+      table.timestamp("updated_at").default(knex.fn.now());
+
+    })
+  }
+}
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists("users")
+};
