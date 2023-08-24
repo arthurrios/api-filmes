eb242db0a34a48c670b014e26e5769be76ab6c07
arthur <arthur.rios007@gmail.com>
2023-08-24 10:42:56 -0300

    add routes folder

---
+++ src/server.js
@@ -0,0 +1,8 @@
+require("express-async-errors");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
