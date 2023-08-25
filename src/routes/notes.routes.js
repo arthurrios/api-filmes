6d49ad89e33200cb18e25b804ee1cb1ff9508b6d
arthur <arthur.rios007@gmail.com>
2023-08-25 00:15:40 -0300

    add all notes methods

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,6 +6,9 @@ const notesRoutes = Router()
 
 const notesController = new NotesController()
 
+notesRoutes.get("/:id", notesController.show)
+notesRoutes.get("/", notesController.index)
 notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.delete("/:id", notesController.delete)
 
 module.exports = notesRoutes
